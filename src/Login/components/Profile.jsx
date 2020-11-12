d2f46edf30dea2aba5324edd986ad97200036b9d
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-12 16:39:48 +0100

    last commit

--- src/Login/components/Profile.jsx
+++ src/Login/components/Profile.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export default function Profile() {
-  const user = useSelector((state) => state.loggedUser);
+  const user = useSelector((state) => state.loggedUser.name);
   return (
     <div>
-      <h1>Welcome {user.name}</h1>
+      <h1>Welcome {user}</h1>
       <p>You are successfully logged in!</p>
     </div>
   );
