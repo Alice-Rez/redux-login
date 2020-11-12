49f4e3ee6c9baba1f1b6af00dc8c8e629ebb5092
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-12 14:58:34 +0100

    added reactstrap and react router

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./Appreverse";
+import App from "./App";
 import { Provider } from "react-redux";
 // import myStore from "./ReverseWord/Redux/store.js";
+import myStore from "./Login/redux/store";
 
 ReactDOM.render(
   <Provider store={myStore}>
