ecfc4d3bf99b08bb3d182c16595c808cbf9e1ecf
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-12 15:07:03 +0100

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SignUp from "./Login/components/SignUp";
 
 export default function App() {
-  return <div>Test</div>;
+  return (
+    <section>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <SignUp />
+          </Route>
+          <Route path="/login"></Route>
+          <Route path="/profile"></Route>
+        </Switch>
+      </Router>
+    </section>
+  );
 }
